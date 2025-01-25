c2040e44798bdd2586228cd82a59b2af5737d7c2
wynprtm <wayanpratama38@gmail.com>
2025-01-25 15:30:54 +0700

    Adding Back to Top Button

--- app/src/main/java/com/example/herolist/EpicSevenApp.kt
+++ app/src/main/java/com/example/herolist/EpicSevenApp.kt
@@ -1,20 +1,38 @@
 package com.example.herolist
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,6 +40,7 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.herolist.models.HeroesData
 import com.example.herolist.ui.theme.HeroListTheme
+import kotlinx.coroutines.launch
 
 
 @Composable
@@ -36,12 +55,13 @@ fun HeroesListItem(
     ){
         AsyncImage(
             model = photoUrl,
+            placeholder = painterResource(R.drawable.a_tywin),
             contentDescription = null,
             contentScale = ContentScale.Crop,
             modifier = Modifier
                 .padding(8.dp)
                 .size(60.dp)
-                .clip(CircleShape)
+                .clip(CircleShape),
         )
         Text(
             text = name,
@@ -54,12 +74,23 @@ fun HeroesListItem(
     }
 }
 
+
+// Main App
 @Composable
 fun EpicSevenApp(
     modifier: Modifier = Modifier
 ){
     Box(modifier = modifier){
-        LazyColumn {
+        val scope = rememberCoroutineScope()
+        val listState = rememberLazyListState()
+        val showButton: Boolean by remember {
+            derivedStateOf { listState.firstVisibleItemIndex > 0 }
+        }
+
+        LazyColumn (
+            state = listState,
+            contentPadding = PaddingValues(bottom = 80.dp)
+        ){
             items(HeroesData.heroes, key = { it.id }){ hero ->
                 HeroesListItem(
                     name = hero.name,
@@ -68,10 +99,41 @@ fun EpicSevenApp(
                 )
             }
         }
+
+        AnimatedVisibility(
+            visible = showButton,
+            enter = fadeIn() + slideInVertically(),
+            exit = fadeOut() + slideOutVertically(),
+            modifier = Modifier
+                .padding(bottom = 30.dp)
+                .align(Alignment.BottomCenter)
+        ) {
+            ScrollToTopButton(
+                onClick = {
+                    scope.launch{
+                        listState.scrollToItem(0)
+                    }
+                }
+            )
+        }
     }
 }
 
-
+@Composable
+fun ScrollToTopButton(
+    onClick : () -> Unit,
+    modifier: Modifier = Modifier
+){
+    FilledTonalButton(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = Icons.Filled.KeyboardArrowUp,
+            contentDescription = stringResource(R.string.scroll_to_top)
+        )
+    }
+}
 
 
 @Preview(showBackground = true, device = Devices.PIXEL_4)
